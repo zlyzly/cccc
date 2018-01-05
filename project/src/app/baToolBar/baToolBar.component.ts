1ec49be66b0e7c1f668ad552fdbf25b400e34b7e
zly <1468236478@qq.com>
2018-01-05 17:35:10 +0800

    使用ngprimeng

--- project/src/app/baToolBar/baToolBar.component.ts
+++ project/src/app/baToolBar/baToolBar.component.ts
@@ -6,12 +6,12 @@ import { BaToolbarDataService } from './baToolBar.service';
 /**
 name:"",  节点名称，目前在组里面有用，在按钮里面没用
 text: `右`,  节点显示的字符串，或者节点的鼠标hover提示，目前在组上没用，在按钮上有用，如果toolbar设置成图标模式，则只作为提示。
-order: 2, 节点在父节点的所有子级节点中的排列顺序
+order: '2', 节点在父节点的所有子级节点中的排列顺序
 status: "3",  是节点的四个状态：隐藏、禁用、启用、锁定
 isCollapse: true,//在外边  是否折叠，true为折叠到下拉，false为不折叠到下拉
 icon: 'ion-arrow-right-c',  节点的图标，目前在组里面没有用，在按钮里面有用
 customer-style: "",  自定义样式对象（可以定义的样式包括：icon-font-size, text-font-size, text-font-color）
-style: '2',  待定
+style: ''2'',  待定
 group-collapse-icon-bottom-height: 0, 组的折叠按钮的高度（这个可以选择一个合适的高度写死，目前不实现可配置）
 group-collapse-icon-right-weight: 0, 组的折叠按钮的宽度（这个可以选择一个合适的宽度写死，目前不实现可配置）
 height: 0, 节点高度
@@ -24,12 +24,13 @@ childs:  子节点
  */
 @Component({
     selector: 'ba-toolbar',
-    templateUrl: './b.html',
+    templateUrl: './baToolBar.html',
     styleUrls: ['./baToolBar.scss'],
 })
 export class BaToolBar {
     // 接收传来的数据。
     newSettings: any;
+    flay: boolean = true;
     @Input() settings: any;
     @Input() datas: any;
     // 从当前组件发散到负组件
@@ -40,93 +41,273 @@ export class BaToolBar {
     dp;
     data: any;
     constructor(private service: BaToolbarDataService) {
-        //     this.service.registerBaToolBar(this.dp);
+        // this.service.registerBaToolBar(this.dp);
         let data = [
             {
-                name: 'action1',
+                name: 'refresh-group',
+                icon: '',
+                text: `刷新`,
+                status: "3",
                 order: 1,
-                status: 1,
-                isCollapse: 0,
-                // 他是一个组
+                style: '2',
+                isCollapse: 0,//在外边
                 childs: [
                     {
-                        icon: 'ion-plus-round',
-                        text: `新增`,
-                        index: 1,
-                        status: 2,
-                        isCollapse: 0,
+                        name: '',
+                        icon: '',
+                        text: '',
+                        order: 1,
+                        status: "3",
+                        style: '2',
+                        isCollapse: 0,//在外边
+                        childs: [
+                            {
+                                name: 'refresh',
+                                icon: 'ion-android-refresh',
+                                text: `刷新`,
+                                order: 1,
+                                status: "3",
+                                style: '2',
+                                isCollapse: 0,//在外边
+                            },
+                            {
+                                name: '定时刷新',
+                                icon: 'ion-load-a',
+                                text: `定时刷新`,
+                                order: 2,
+                                status: "3",
+                                style: '1',
+                                isCollapse: 0,//在外边
+                            },
+                        ]
+                    }
+                ]
+            },
+            {
+                name: 'search-group',
+                icon: '',
+                text: `查询`,
+                status: "3",
+                order: 1,
+                style: '2',
+                isCollapse: 0,//在外边
+                childs: [
+                    {
+                        name: '查询',
+                        icon: 'ion-ios-search',
+                        text: '查询',
+                        order: 1,
+                        status: "3",
+                        style: '2',
+                        isCollapse: 0,//在外边
+                    }
+                ]
+            },
+            {
+                name: 'add-group',
+                icon: '',
+                text: `新增`,
+                status: "3",
+                order: 1,
+                style: '2',
+                isCollapse: 0,//在外边
+                childs: [
+                    {
+                        name: '',
+                        icon: '',
+                        text: '',
+                        order: 1,
+                        status: "3",
+                        style: '2',
+                        isCollapse: 0,//在外边
+                        childs: [
+                            {
+                                name: '新增',
+                                icon: 'ion-android-add',
+                                text: `新增`,
+                                order: 1,
+                                status: "3",
+                                style: '2',
+                                isCollapse: 0,//在外边
+                            },
+                            {
+                                name: '复制',
+                                icon: 'fa fa-files-o',
+                                text: `复制`,
+                                order: 2,
+                                status: "3",
+                                style: '1',
+                                isCollapse: 0,//在外边
+                            },
+                        ]
+                    }
+                ]
+            },
+            {
+                name: 'print-group',
+                icon: '',
+                text: `打印`,
+                status: "3",
+                order: 1,
+                style: '2',
+                isCollapse: 0,//在外边
+                childs: [
+                    {
+                        name: '',
+                        icon: '',
+                        text: '',
+                        order: 1,
+                        status: "3",
+                        style: '2',
+                        isCollapse: 0,//在外边
+                        childs: [
+                            {
+                                name: '打印',
+                                icon: 'ion-android-print',
+                                text: `打印`,
+                                order: 1,
+                                status: "3",
+                                style: '2',
+                                isCollapse: 0,//在外边
+                            },
+                            {
+                                name: '预览',
+                                icon: 'ion-ios-eye',
+                                text: `预览`,
+                                order: 2,
+                                status: "3",
+                                style: '1',
+                                isCollapse: 0,//在外边
+                            },
+                            {
+                                name: '设计',
+                                icon: 'ion-social-designernews',
+                                text: `设计`,
+                                order: 2,
+                                status: "3",
+                                style: '1',
+                                isCollapse: 0,//在外边
+                            },
+                        ]
+                    }
+                ]
+            },
+            {
+                name: '修改',
+                icon: '',
+                text: `修改`,
+                status: "3",
+                order: 1,
+                style: '2',
+                isCollapse: 0,//在外边
+                childs: [
+                    {
+                        name: '',
+                        icon: '',
+                        text: '',
+                        order: 1,
+                        status: "3",
+                        style: '2',
+                        isCollapse: 0,//在外边
+                        childs: [
+                            {
+                                name: '修改',
+                                icon: 'ion-edit',
+                                text: `修改`,
+                                order: 1,
+                                status: "3",
+                                style: '2',
+                                isCollapse: 0,//在外边
+                            },
+                            {
+                                name: '作废',
+                                icon: 'ion-android-delete',
+                                text: `作废`,
+                                order: 2,
+                                status: "3",
+                                style: '1',
+                                isCollapse: 0,//在外边
+                            },
+                        ]
+                    }
+                ]
+            },
+            {
+                name: '',
+                icon: '',
+                text: ``,
+                status: "3",
+                order: 1,
+                style: '2',
+                isCollapse: 0,//在外边
+                childs: [
+                    {
+                        name: '配置',
+                        icon: 'ion-gear-b',
+                        text: '配置',
+                        order: 1,
+                        status: "3",
+                        style: '2',
+                        isCollapse: 0,//在外边
                     },
                     {
-                        icon: 'ion-edit',
-                        text: `修改`,
-                        index: 0,
-                        status: 1,
-                        isCollapse: 1,
+                        name: '退出',
+                        icon: 'ion-android-exit',
+                        text: '退出',
+                        order: 1,
+                        status: "3",
+                        style: '2',
+                        isCollapse: 0,//在外边
                     },
                     {
-                        icon: 'ion-trash-a',
-                        text: `删除`,
-                        index: 3,
-                        status: 1,
-                        isCollapse: 0,
-                    },
-                    {
-                        icon: 'ion-trash-a',
-                        text: `删除1`,
-                        index: 3,
-                        status: 1,
-                        isCollapse: 1,
+                        name: '列表',
+                        icon: 'ion-android-list',
+                        text: '列表',
+                        order: 1,
+                        status: "3",
+                        style: '2',
+                        isCollapse: 0,//在外边
                     },
                 ]
             },
             {
-                name: 'action2',
+                name: '提交',
+                icon: '',
+                text: `提交`,
+                status: "3",
                 order: 1,
-                status: 1,
-                isCollapse: 0,
+                style: '2',
+                isCollapse: 0,//在外边
                 childs: [
                     {
-                        icon: 'ion-plus-round',
-                        text: `新增`,
-                        index: 1,
-                        status: 1,
-                        isCollapse: 1,
-                    },
-                    {
-                        name: `修改`,
-                        index: 0,
-                        status: 2,
-                        isCollapse: 0,
-                        childs: [{
-                            icon: 'ion-plus-round',
-                            text: `新增`,
-                            index: 1,
-                            status: 1,
-                            isCollapse: 0,
-                        },
-                        {
-                            icon: 'ion-plus-round',
-                            text: `新增1`,
-                            index: 1,
-                            status: 1,
-                            isCollapse: 1,
-                        },
-                        {
-                            icon: 'ion-plus-round',
-                            text: `新增2`,
-                            index: 1,
-                            status: 1,
-                            isCollapse: 1,
-                        }
+                        name: '',
+                        icon: '',
+                        text: '',
+                        order: 1,
+                        status: "3",
+                        style: '2',
+                        isCollapse: 0,//在外边
+                        childs: [
+                            {
+                                name: '提交',
+                                icon: 'ion-android-done',
+                                text: `提交`,
+                                order: 1,
+                                status: "3",
+                                style: '2',
+                                isCollapse: 0,//在外边
+                            },
+                            {
+                                name: '撤销',
+                                icon: 'ion-ios-undo',
+                                text: `撤销`,
+                                order: 2,
+                                status: "3",
+                                style: '1',
+                                isCollapse: 0,//在外边
+                            },
                         ]
-                    },
-                    {
-                        icon: 'ion-trash-a',
-                        text: `删除`,
-                        index: 3,
-                        status: 2,
-                        isCollapse: 0,
-                    },
+                    }
                 ]
             },
         ]
@@ -141,12 +322,10 @@ export class BaToolBar {
         if (event.status == '4') {
             window.confirm('需要权限！');
         }
-        
+
         this.toolbarItemClick.emit(event);
     }
-open(child){
-console.log(child)
-}
-
-
+    open(child) {
+        console.log(child)
+    }
 }
--- project/src/app/primeng/primeng.component.ts
+++ project/src/app/primeng/primeng.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CarService } from './carService';
 @Component({
   selector: 'app-primeng',
   templateUrl: './primeng.component.html',
-  styleUrls: ['./primeng.component.css']
+  styleUrls: ['./primeng.component.scss']
 })
 export class PrimengComponent implements OnInit {
-
-  constructor() { }
+  cars: object[] = [];
+  sales: any[];
+  tree: object[] = [];
+  files: object[] = [];
+  constructor(private carService: CarService) { }
 
   ngOnInit() {
+    // this.cars = [
+    //   { "brand": "VW", "year": 2012, "color": "Orange", "vin": "dsad231ff" },
+    //   { "brand": "Audi", "year": 2011, "color": "Black", "vin": "gwregre345" },
+    //   { "brand": "Renault", "year": 2005, "color": "Gray", "vin": "h354htr" },
+    //   { "brand": "BMW", "year": 2003, "color": "Blue", "vin": "j6w54qgh" },
+    //   { "brand": "Mercedes", "year": 1995, "color": "Orange", "vin": "hrtwy34" },
+    //   { "brand": "Volvo", "year": 2005, "color": "Black", "vin": "jejtyj" },
+    //   { "brand": "Honda", "year": 2012, "color": "Yellow", "vin": "g43gr" },
+    //   { "brand": "Jaguar", "year": 2013, "color": "Orange", "vin": "greg34" },
+    //   { "brand": "Ford", "year": 2000, "color": "Black", "vin": "h54hw5" },
+    //   { "brand": "Fiat", "year": 2013, "color": "Red", "vin": "245t2s" }
+    // ];
+    // // console.log(this.cars);
+    // this.sales = [
+    //   { brand: 'Apple', lastYearSale: '51%', thisYearSale: '40%', lastYearProfit: '$54,406.00', thisYearProfit: '$43,342' },
+    //   { brand: 'Samsung', lastYearSale: '83%', thisYearSale: '96%', lastYearProfit: '$423,132', thisYearProfit: '$312,122' },
+    //   { brand: 'Microsoft', lastYearSale: '38%', thisYearSale: '5%', lastYearProfit: '$12,321', thisYearProfit: '$8,500' },
+    //   { brand: 'Philips', lastYearSale: '49%', thisYearSale: '22%', lastYearProfit: '$745,232', thisYearProfit: '$650,323,' },
+    //   { brand: 'Song', lastYearSale: '17%', thisYearSale: '79%', lastYearProfit: '$643,242', thisYearProfit: '500,332' },
+    //   { brand: 'LG', lastYearSale: '52%', thisYearSale: ' 65%', lastYearProfit: '$421,132', thisYearProfit: '$150,005' },
+    //   { brand: 'Sharp', lastYearSale: '82%', thisYearSale: '12%', lastYearProfit: '$131,211', thisYearProfit: '$100,214' },
+    //   { brand: 'Panasonic', lastYearSale: '44%', thisYearSale: '45%', lastYearProfit: '$66,442', thisYearProfit: '$53,322' },
+    //   { brand: 'HTC', lastYearSale: '90%', thisYearSale: '56%', lastYearProfit: '$765,442', thisYearProfit: '$296,232' },
+    //   { brand: 'Toshiba', lastYearSale: '75%', thisYearSale: '54%', lastYearProfit: '$21,212', thisYearProfit: '$12,533' }
+    // ];
+    this.sales = this.carService.getData().data;
+    this.tree = this.carService.getTree();
+    this.files = this.carService.gettree1();
   }
 
 }
